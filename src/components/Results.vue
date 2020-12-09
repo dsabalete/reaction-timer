967a65dcc7185d4615474ff497ea6247a9547e73
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2020-12-09 21:49:43 +0100

    feat: add Results and event logic

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,41 @@
 <template>
-    <div>
-        Results
+    <div class="results">
+        <p class="rank">{{ rank }}</p>
+        <p>Reaction time: {{ score }} ms</p>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.results {
+    margin: 60px auto;
+    padding: 60px;
+    color: #0faf87;
+    border: 2px solid #0faf87;
+    border-radius: 20px;
+    width: 400px;
+}
+.rank {
+    font-size: 1.4em;
+    font-weight: bold;
+}
+</style>
